2e03e48e15472a47fb2d670975b0ebbede5ff407
Alex Sea <seagullalex81@gmail.com>
2025-02-05 22:30:14 +0200

    test

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -1,16 +1,24 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-axios.defaults.baseURL = "https://connections-api.goit.global/";
+// axios.defaults.baseURL = "https://connections-api.goit.global/";
+
+export const goitApi = axios.create({
+    baseURL: "https://connections-api.goit.global/",
+});
+
+const setAuthHeader = token => {
+    axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+};
 
 export const registerThunk = createAsyncThunk(
     "auth/register",
     async (credentials, thunkApi) => {
         try {
-            const { data } = await axios.post("/users/signup", credentials);
+            const { data } = await goitApi.post("/users/signup", credentials);
             const { token } = data;
             localStorage.setItem('token', token);
-            axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+            setAuthHeader(token);
             console.log(data);
             return data;
         } catch (error) {
@@ -24,10 +32,10 @@ export const loginThunk = createAsyncThunk(
     "auth/login",
     async (credentials, thunkApi) => {
         try {
-            const { data } = await axios.post("/users/login", credentials);
+            const { data } = await goitApi.post("/users/login", credentials);
             const { token } = data;
             localStorage.setItem('token', token);
-            axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+            setAuthHeader(token);
             console.log(data);
             return data;
         } catch (error) {
@@ -42,7 +50,7 @@ export const logoutThunk = createAsyncThunk(
         try {
             await axios.post('/users/logout');
             localStorage.removeItem("token");
-            delete axios.defaults.headers.common["Authorization"];
+            delete goitApi.defaults.headers.common["Authorization"];
             return;
         } catch (error) {
             return thunkApi.rejectWithValue(error.message);
