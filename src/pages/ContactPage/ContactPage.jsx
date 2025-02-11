2edfc1ed3f8de832368aacdd2fafbcbef44749b5
Alex Sea <seagullalex81@gmail.com>
2025-02-11 20:54:08 +0200

    test

--- src/pages/ContactPage/ContactPage.jsx
+++ src/pages/ContactPage/ContactPage.jsx
@@ -4,17 +4,21 @@ import ContactList from "../../components/ContactList/ContactList";
 import SearchBox from "../../components/SearchBox/SearchBox";
 import { fetchDataThunk } from "../../redux/contacts/operations";
 import { useDispatch, useSelector } from "react-redux";
-import { selectIsLoggedIn } from "../../redux/auth/selectors";
+import {
+  selectIsLoggedIn,
+  selectIsRefreshing,
+} from "../../redux/auth/selectors";
 
 const ContactPage = () => {
   const dispatch = useDispatch();
   const isLoggedIn = useSelector(selectIsLoggedIn);
+  const isRefreshing = useSelector(selectIsRefreshing);
 
   useEffect(() => {
-    if (isLoggedIn) {
+    if (isLoggedIn && !isRefreshing) {
       dispatch(fetchDataThunk());
     }
-  }, [dispatch, isLoggedIn]);
+  }, [dispatch, isLoggedIn, isRefreshing]);
 
   return (
     <div>
